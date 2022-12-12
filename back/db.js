fce4a99945e06414502822d6d58f3364ab9a8da8
Otsukionata <e-ramassamy@laposte.net>
2022-12-12 12:01:27 +0100

    mise à jour variable d'environnement

--- back/db.js
+++ back/db.js
@@ -1,14 +1,16 @@
-require("dotenv").config()
+require("dotenv").config();
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const password = process.env.DB_PASSWORD;
 const username = process.env.DB_USER;
 const db = process.env.DB_NAME;
-const uri = `mongodb+srv://${username}:${password}@${db}.iqrq2k7.mongodb.net/?retryWrites=true&w=majority`;
+const cluster = process.env.DB_CLUSTER;
+const uri = `mongodb+srv://${username}:${password}@${db}.${cluster}.mongodb.net/?retryWrites=true&w=majority`;
 
-mongoose.connect(uri)
-    .then(() => console.log("Connexion à MongoDB réussie !"))
-    .catch((err) => console.error("Échec de connexion à MongoDB…", err))
+mongoose
+  .connect(uri)
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch((err) => console.error("Échec de connexion à MongoDB…", err));
 
 module.exports = { mongoose };
