4f063f7ba69e715caa9bf5e7313df08de5cd5627
Otsukionata <e-ramassamy@laposte.net>
2022-12-15 15:41:53 +0100

    Ajout commentaires fichier db

--- back/db.js
+++ back/db.js
@@ -2,12 +2,16 @@ require("dotenv").config();
 
 const mongoose = require("mongoose");
 
+// Import des données enregistrées en variables d'environnement
 const password = process.env.DB_PASSWORD;
 const username = process.env.DB_USER;
 const db = process.env.DB_NAME;
 const cluster = process.env.DB_CLUSTER;
+
+// Identification pour connexion au serveur
 const uri = `mongodb+srv://${username}:${password}@${db}.${cluster}.mongodb.net/?retryWrites=true&w=majority`;
 
+// Cas de réussite ou d'échec de connexion
 mongoose
   .connect(uri)
   .then(() => console.log("Connexion à MongoDB réussie !"))
