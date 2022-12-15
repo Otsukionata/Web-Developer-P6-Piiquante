bd51172a0a9931bbb3968e373bfc70cd0e234d63
Otsukionata <e-ramassamy@laposte.net>
2022-12-15 16:10:48 +0100

    correction contrôleur mail

--- back/middleware/email-check.js
+++ back/middleware/email-check.js
@@ -3,7 +3,7 @@ const validator = require("validator");
 module.exports = (req, res, next) => {
     const email = req.body.email;
 
-    if (validator.isEmail()) {
+    if (validator.isEmail(email)) {
         next();
     } else {
         return res.status(400).json({message: "L'adresse " + email + " est invalide."})
